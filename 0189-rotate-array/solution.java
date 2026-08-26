6bbda977dd0628e9e80f0306d2844b8b663b67df
Glen Andrew Rodrigues <glenandrew.7r@gmail.com>
2026-08-26 17:51:57 +0000

    Sync LeetCode submission Runtime - 4 ms (17.42%), Memory - 267 MB (13.13%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -8,18 +8,17 @@ class Solution
             nums[start] = nums[end];
             nums[end] = temp;
 
-            start += 1;
-            end -= 1;
+            start++;
+            end--;
         }
     }
 
     public void rotate(int[] nums, int k) 
     {
-        int n = nums.length;
-        k %= n;
+        k %= nums.length;
 
-        reverse(nums, 0, n - 1);
+        reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, (n - 1));
+        reverse(nums, k, (nums.length - 1));
     }
 }
