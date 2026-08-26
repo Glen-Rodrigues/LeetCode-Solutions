815880d6b63d6c9ca92554603b20915d39c11349
Glen Andrew Rodrigues <glenandrew.7r@gmail.com>
2026-08-26 18:23:09 +0000

    Sync LeetCode submission Runtime - 139 ms (13.85%), Memory - 35.2 MB (9.02%)

---
+++ 0189-rotate-array/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        k %= len(nums)
+        if k == 0:
+            return
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+
+                start += 1
+                end -= 1
+        
+        reverse(0, len(nums) - 1)
+        reverse(0, k -1)
+        reverse(k, len(nums) - 1)
+        
+        
+        
